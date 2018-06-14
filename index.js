091084d215970c961e1d0a18d2356540b10a3144
jirov <jirov.dev@gmail.com>
2018-06-14 14:25:02 +0500

    JiroVChat

--- index.js
+++ index.js
@@ -11,16 +11,34 @@ app.use(express.static('public'));
 // Socket setup
 const io = socket(server);
 
+var users = [];
+var connections = [];
+
 // Listen event connection
 io.on('connection', (socket) => {
-    console.log(`Socket connection ${socket.id}`);
+    connections.push(socket);
+    console.log(`${connections.length} persons has connected!`);
 
     socket.on('chat', (data) => {
         io.sockets.emit('chat', data);
     });
 
-    socket.on('typing',(data)=>{
-        socket.broadcast.emit('typing',data);
+    socket.on('typing', (data) => {
+        socket.broadcast.emit('typing', data);
     });
-}
-);
+
+    // Disconnect
+    socket.on('disconnect', (data) => {
+        connections.splice(connections.indexOf(socket), 1);
+        users.splice(users.indexOf(socket.username),1);
+        console.log(users);
+        io.sockets.emit('newuser',users);
+        console.log(`${connections.length} persons has connected!`);
+    });
+
+    socket.on('newuser', (data) => {
+        socket.username = data;
+        users.push(data);
+        io.sockets.emit('newuser',users);
+    });
+});
--- public/chat.js
+++ public/chat.js
@@ -1,38 +1,69 @@
 window.onload = () => {
     // Connection
-    const socket = io.connect('https://jirochat.herokuapp.com/');
+    const socket = io.connect('http://localhost:3000/');
 
     var output = document.getElementById("output");
-    var name = document.getElementById("name");
+    var name;
     var message = document.getElementById("message");
     var send = document.getElementById('send');
     var typing = document.getElementById('typing');
+    var username = document.getElementById('username');
+    var login = document.getElementById('login');
+    var formLogin = document.getElementById('formlogin');
+    var formChat = document.getElementById('formchat');
+    var listUsers = document.getElementById('list-users');
 
     //Emit events
     send.addEventListener('click', () => {
         socket.emit('chat', {
-            name: name.value,
+            name: name,
             message: message.value
         });
-        output.innerHTML += `<p style="text-align:right"><span>${message.value}</span><strong> ${name.value}</strong></p>`;
+        output.innerHTML += `<p style="text-align:right"><span>${message.value}</span><strong> ${name}</strong></p>`;
         message.value = "";
     });
 
     message.addEventListener('keypress', () => {
-        socket.emit('typing', name.value);
-    })
+        socket.emit('typing', name);
+    });
 
     // Listen Events
     socket.on('chat', (data) => {
-        if (data.name != name.value) {
+        if (data.name != name) {
             output.innerHTML += `<p style="text-align:left"><strong>${data.name} </strong><span>${data.message}</span></p>`;
         }
         document.getElementById("typing").innerHTML = "";
     });
 
     socket.on('typing', (data) => {
-        if (data != name.value) {
+        if (data != name) {
             typing.innerHTML = `<p style="text-align:left"><em><strong>${data}</strong> is writing message ... </em></p>`;
         }
-    })
+    });
+
+    login.addEventListener('click', (e) => {
+        e.preventDefault();
+        socket.emit('newuser', username.value);
+        if (username.value != '') {
+            console.log("zo day");
+            formLogin.style.display = "none";
+            formChat.style.display = "block";
+
+            name = username.value;
+        }
+        else{
+            alert('Username is required!');
+            username.style.border = "1px solid red";
+        }
+        document.getElementById('name').textContent += `User : ${name}`;
+        username.value = "";
+    });
+
+    socket.on('newuser', (data) => {
+        listUsers.innerHTML = "";
+        console.log(`Data : ${data}`);
+        data.forEach((item) => {
+            listUsers.innerHTML += `<li class="list-group-item"><span class="glyphicon glyphicon-user"></span> <strong>${item}</strong></li>`
+        });
+    });
 }
